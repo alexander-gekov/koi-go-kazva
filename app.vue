4c2388de3f5c80b87a99d886f2c1267babc8e9bd
Aleksandar Gekov <alexander.gekov00@gmail.com>
2023-03-12 17:49:38 +0100

    make more mobile-friendly

--- app.vue
+++ app.vue
@@ -1,10 +1,10 @@
 <template>
-  <div class="flex justify-center items-center h-full p-10">
-    <div class="w-2/3 h-full border-gray border p-4 shadow-xl rounded-lg flex flex-col justify-between">
+  <div class="flex justify-center items-center h-full p-2 lg:p-10">
+    <div class="w-full lg:w-2/3 h-full border-gray border p-4 shadow-xl rounded-lg flex flex-col justify-between">
       <h2 class="text-2xl text-center font-bold mb-5">Кой го казва?</h2>
       <p class="text-right my-2">Резултат: {{ round }}</p>
       <p class="bg-gray-200 text-center w-full flex-1 mx-auto px-4 py-2 mb-10">"{{ currentQuote.quote }}"</p>
-      <div class="grid grid-cols-3 gap-4">
+      <div class="grid grid-cols-1 lg:grid-cols-3 gap-4">
         <button @click="select(person.id)" v-for="person in people" :key="person.id" :disabled="selectedPerson" 
         :class="{'border-gray-700': person.id == selectedPerson, 'border-red-500': gameOver && person.id == currentQuote.said_by}" 
         class="border-2 transition duration-500 hover:[&:not(:disabled)]:border-gray-700 border-gray-300 disabled:cursor-not-allowed disabled:text-gray-700 p-4 rounded-lg text-center">
