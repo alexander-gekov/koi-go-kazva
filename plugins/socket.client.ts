004b4aa9c63890e2eb3858378887110a18b52993
Alexander Gekov <alexander.gekov00@gmail.com>
2023-03-15 13:50:05 +0100

    update

--- plugins/socket.client.ts
+++ plugins/socket.client.ts
@@ -1,7 +1,8 @@
 import io from 'socket.io-client'
 
 export default defineNuxtPlugin(() => {
-    const socket = io('https://koi-go-kazva-ws-gvgzl.ondigitalocean.app:8080', {secure: true, rejectUnauthorized: false})
+    const socket = io('https://koi-go-kazva-ws-gvgzl.ondigitalocean.app:8080', {transports: 
+    ['http'], reconnection: false,secure:false})
 
     return {
         provide: {
