3fc45a9ae0a38a14a108e78b484cf05f42e9fc2d
Alexander Gekov <alexander.gekov00@gmail.com>
2023-03-15 15:59:01 +0100

    change url again

--- plugins/socket.client.ts
+++ plugins/socket.client.ts
@@ -1,7 +1,7 @@
 import io from 'socket.io-client'
 
 export default defineNuxtPlugin(() => {
-    const socket = io('https://koi-go-kazva-rqk73.ondigitalocean.app', {reconnection: false, secure:false, rejectUnauthorized: false})
+    const socket = io('https://koi-go-kazva-rqk73.ondigitalocean.app:3000', {reconnection: false, secure:false, rejectUnauthorized: false})
 
     return {
         provide: {
