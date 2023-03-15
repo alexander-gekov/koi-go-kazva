20baf5d71ca8c0bae362e03cda5ee44d19729687
Alexander Gekov <alexander.gekov00@gmail.com>
2023-03-15 10:51:56 +0100

    try again

--- plugins/socket.client.ts
+++ plugins/socket.client.ts
@@ -1,7 +1,7 @@
 import io from 'socket.io-client'
 
 export default defineNuxtPlugin(() => {
-    const socket = io('http://localhost:3000')
+    const socket = io();
 
     return {
         provide: {
