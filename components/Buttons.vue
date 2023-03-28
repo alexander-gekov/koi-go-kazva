d040221572aa66abff2f09be7b7526dad96f59ba
Aleksandar Gekov <alexander.gekov00@gmail.com>
2023-03-28 23:57:21 +0200

    only 2 options, banner + fix multi

--- components/Buttons.vue
+++ components/Buttons.vue
@@ -1,31 +1,110 @@
 <template>
-    <button class="border border-gray-300 hover:border-gray-500 dark:hover:border-slate-600 dark:border-[#1f1e1e] p-2 rounded-xl mr-2" @click="$emit('linkGame')">
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M13.19 8.688a4.5 4.5 0 011.242 7.244l-4.5 4.5a4.5 4.5 0 01-6.364-6.364l1.757-1.757m13.35-.622l1.757-1.757a4.5 4.5 0 00-6.364-6.364l-4.5 4.5a4.5 4.5 0 001.242 7.244" />
-        </svg>
-    </button>
-    <button class="border border-gray-300 hover:border-gray-500 dark:hover:border-slate-600 dark:border-[#1f1e1e] p-2 rounded-xl" @click="$emit('useHint')">
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M12 18v-5.25m0 0a6.01 6.01 0 001.5-.189m-1.5.189a6.01 6.01 0 01-1.5-.189m3.75 7.478a12.06 12.06 0 01-4.5 0m3.75 2.383a14.406 14.406 0 01-3 0M14.25 18v-.192c0-.983.658-1.823 1.508-2.316a7.5 7.5 0 10-7.517 0c.85.493 1.509 1.333 1.509 2.316V18" />
-        </svg>
-    </button>
-    <span class="h-10 border dark:border-[#1f1e1e] mx-2"></span>
-    <button class="border border-gray-300 hover:border-gray-500 dark:hover:border-slate-600 dark:border-[#1f1e1e] p-2 rounded-xl mr-2" @click="$emit('toggleColorMode')">
-        <svg v-if="$colorMode.preference === 'light'" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" class="w-6 h-6">
-            <path strokeLinecap="round" strokeLinejoin="round" d="M21.752 15.002A9.718 9.718 0 0118 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 003 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 009.002-5.998z" />
-        </svg>
-        <svg v-else xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M12 3v2.25m6.364.386l-1.591 1.591M21 12h-2.25m-.386 6.364l-1.591-1.591M12 18.75V21m-4.773-4.227l-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z" />
-        </svg>
-    </button>
-    <button class="border border-gray-300 hover:border-gray-500 dark:hover:border-slate-600 dark:border-[#1f1e1e] p-2 rounded-xl" @click="$emit('openSettings')">
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M9.594 3.94c.09-.542.56-.94 1.11-.94h2.593c.55 0 1.02.398 1.11.94l.213 1.281c.063.374.313.686.645.87.074.04.147.083.22.127.324.196.72.257 1.075.124l1.217-.456a1.125 1.125 0 011.37.49l1.296 2.247a1.125 1.125 0 01-.26 1.431l-1.003.827c-.293.24-.438.613-.431.992a6.759 6.759 0 010 .255c-.007.378.138.75.43.99l1.005.828c.424.35.534.954.26 1.43l-1.298 2.247a1.125 1.125 0 01-1.369.491l-1.217-.456c-.355-.133-.75-.072-1.076.124a6.57 6.57 0 01-.22.128c-.331.183-.581.495-.644.869l-.213 1.28c-.09.543-.56.941-1.11.941h-2.594c-.55 0-1.02-.398-1.11-.94l-.213-1.281c-.062-.374-.312-.686-.644-.87a6.52 6.52 0 01-.22-.127c-.325-.196-.72-.257-1.076-.124l-1.217.456a1.125 1.125 0 01-1.369-.49l-1.297-2.247a1.125 1.125 0 01.26-1.431l1.004-.827c.292-.24.437-.613.43-.992a6.932 6.932 0 010-.255c.007-.378-.138-.75-.43-.99l-1.004-.828a1.125 1.125 0 01-.26-1.43l1.297-2.247a1.125 1.125 0 011.37-.491l1.216.456c.356.133.751.072 1.076-.124.072-.044.146-.087.22-.128.332-.183.582-.495.644-.869l.214-1.281z" />
-            <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
-        </svg>
-    </button>
+  <button
+    title="Играй с приятел"
+    class="border border-gray-300 hover:border-gray-500 dark:hover:border-slate-600 dark:border-[#1f1e1e] p-2 rounded-xl mr-2 flex"
+    @click="$emit('linkGame')"
+  >
+    Играй с приятел
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke-width="1.5"
+      stroke="currentColor"
+      class="ml-2 w-6 h-6"
+    >
+      <path
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        d="M13.19 8.688a4.5 4.5 0 011.242 7.244l-4.5 4.5a4.5 4.5 0 01-6.364-6.364l1.757-1.757m13.35-.622l1.757-1.757a4.5 4.5 0 00-6.364-6.364l-4.5 4.5a4.5 4.5 0 001.242 7.244"
+      />
+    </svg>
+  </button>
+  <button
+    title="Използвай жокер"
+    class="border border-gray-300 hover:border-gray-500 dark:hover:border-slate-600 dark:border-[#1f1e1e] p-2 rounded-xl flex"
+    @click="$emit('useHint')"
+  >
+    Използвай жокер
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke-width="1.5"
+      stroke="currentColor"
+      class="ml-2 w-6 h-6"
+    >
+      <path
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        d="M12 18v-5.25m0 0a6.01 6.01 0 001.5-.189m-1.5.189a6.01 6.01 0 01-1.5-.189m3.75 7.478a12.06 12.06 0 01-4.5 0m3.75 2.383a14.406 14.406 0 01-3 0M14.25 18v-.192c0-.983.658-1.823 1.508-2.316a7.5 7.5 0 10-7.517 0c.85.493 1.509 1.333 1.509 2.316V18"
+      />
+    </svg>
+  </button>
+  <span class="h-10 border dark:border-[#1f1e1e] mx-2"></span>
+  <button
+    title="Тъмен/Светъл Режим"
+    class="border border-gray-300 hover:border-gray-500 dark:hover:border-slate-600 dark:border-[#1f1e1e] p-2 rounded-xl mr-2"
+    @click="$emit('toggleColorMode')"
+  >
+    <svg
+      v-if="$colorMode.preference === 'light'"
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth="{1.5}"
+      stroke="currentColor"
+      class="w-6 h-6"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M21.752 15.002A9.718 9.718 0 0118 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 003 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 009.002-5.998z"
+      />
+    </svg>
+    <svg
+      v-else
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke-width="1.5"
+      stroke="currentColor"
+      class="w-6 h-6"
+    >
+      <path
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        d="M12 3v2.25m6.364.386l-1.591 1.591M21 12h-2.25m-.386 6.364l-1.591-1.591M12 18.75V21m-4.773-4.227l-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z"
+      />
+    </svg>
+  </button>
+  <button
+    title="Настройки"
+    class="border border-gray-300 hover:border-gray-500 dark:hover:border-slate-600 dark:border-[#1f1e1e] p-2 rounded-xl"
+    @click="$emit('openSettings')"
+  >
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke-width="1.5"
+      stroke="currentColor"
+      class="w-6 h-6"
+    >
+      <path
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        d="M9.594 3.94c.09-.542.56-.94 1.11-.94h2.593c.55 0 1.02.398 1.11.94l.213 1.281c.063.374.313.686.645.87.074.04.147.083.22.127.324.196.72.257 1.075.124l1.217-.456a1.125 1.125 0 011.37.49l1.296 2.247a1.125 1.125 0 01-.26 1.431l-1.003.827c-.293.24-.438.613-.431.992a6.759 6.759 0 010 .255c-.007.378.138.75.43.99l1.005.828c.424.35.534.954.26 1.43l-1.298 2.247a1.125 1.125 0 01-1.369.491l-1.217-.456c-.355-.133-.75-.072-1.076.124a6.57 6.57 0 01-.22.128c-.331.183-.581.495-.644.869l-.213 1.28c-.09.543-.56.941-1.11.941h-2.594c-.55 0-1.02-.398-1.11-.94l-.213-1.281c-.062-.374-.312-.686-.644-.87a6.52 6.52 0 01-.22-.127c-.325-.196-.72-.257-1.076-.124l-1.217.456a1.125 1.125 0 01-1.369-.49l-1.297-2.247a1.125 1.125 0 01.26-1.431l1.004-.827c.292-.24.437-.613.43-.992a6.932 6.932 0 010-.255c.007-.378-.138-.75-.43-.99l-1.004-.828a1.125 1.125 0 01-.26-1.43l1.297-2.247a1.125 1.125 0 011.37-.491l1.216.456c.356.133.751.072 1.076-.124.072-.044.146-.087.22-.128.332-.183.582-.495.644-.869l.214-1.281z"
+      />
+      <path
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+      />
+    </svg>
+  </button>
 </template>
 
 <script setup>
-defineEmits(['linkGame', 'toggleColorMode', 'useHint', 'openSettings'])
+defineEmits(["linkGame", "toggleColorMode", "useHint", "openSettings"]);
 </script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,217 +1,348 @@
 <template>
-    <div class="dark:bg-[#121212] min-h-fit flex justify-center items-center p-2 lg:p-10">
-      <div class="w-full lg:w-2/3 h-full dark:border-[#1f1e1e] border-gray border p-4 shadow-xl rounded-lg flex flex-col">
-        <div class="relative flex-col lg:flex-row flex justify-center items-center dark:text-slate-300">
-          <h2 class="text-2xl text-center font-bold mb-5">Кой го казва?</h2>
-          <div class="lg:absolute lg:top-0 lg:right-0 flex">
-            <Buttons @linkGame="linkGame" @toggleColorMode="toggleColorMode" @useHint="useHint" @openSettings="openSettings"/>
-          </div>
-          <div class="lg:absolute mt-2 lg:mt-0 lg:top-0 lg:left-0" v-if="showRoomId">
-            <div>Игра: {{ router.currentRoute.value.query.g }}</div>
-            <div class="grid grid-cols-3" v-for="player in players" :key="player.name">🔴 {{ player.name }} - {{ player.score }}</div>
-          </div>
+  <div
+    class="dark:bg-[#121212] min-h-fit flex justify-center items-center p-2 lg:p-10"
+  >
+    <div
+      class="w-full lg:w-2/3 h-full dark:border-[#1f1e1e] border-gray border p-4 shadow-xl rounded-lg flex flex-col"
+    >
+      <div
+        class="relative flex-col lg:flex-row flex justify-center items-center dark:text-slate-300"
+      >
+        <h2
+          @click="goToHome"
+          class="text-2xl text-center font-bold mb-5 cursor-pointer"
+        >
+          Кой го казва?
+        </h2>
+        <div class="lg:absolute lg:top-0 lg:right-0 flex">
+          <Buttons
+            @linkGame="linkGame"
+            @toggleColorMode="toggleColorMode"
+            @useHint="useHint"
+            @openSettings="openSettings"
+          />
         </div>
-        <p class="dark:text-slate-300 text-center lg:text-right my-2">Резултат: <span class="font-bold text-lg">{{ round }}</span></p>
-        <p class="dark:bg-[#1f1e1e] dark:text-slate-300 bg-gray-200 text-center w-full mx-auto px-4 py-2 mb-10">"{{ currentQuote?.quote }}"</p>
-        <div class="grid grid-cols-1 lg:grid-cols-3 gap-4">
-          <button :ref="'button' + person.id" @click="select(person.id)" v-for="person in people" :key="person.id" :disabled="selectedPerson != null || (hintUsed && person.id != currentQuote?.said_by && person.id != randomWrongPerson?.id)" 
-          :class="{'border-gray-700 dark:border-white': person.id == selectedPerson, '!border-red-500 !dark:border-[#752828]': gameOver && person.id == currentQuote?.said_by, 'dark:bg-[#1f1f1f] line-through transform scale-50': hintUsed && person.id != currentQuote?.said_by && person.id != randomWrongPerson?.id}" 
-          class="border-2 transition duration-300 dark:border-gray-700 dark:hover:[&:not(:disabled)]:border-white hover:[&:not(:disabled)]:border-gray-700 border-gray-300 disabled:cursor-not-allowed disabled:text-gray-700 p-4 rounded-lg text-center dark:text-slate-300">
-            {{ person.name }} - 
-            <span>{{ person.party }}</span>
-          </button>
-          <button v-if="isCorrect" @click="nextQuote" class="dark:bg-[#287543] bg-green-600 text-white dark:text-slate-200 p-4 rounded-lg text-center">Следващ цитат</button>
-          <button v-else-if="!isCorrect && gameOver" @click="newGame" class="dark:bg-[#1f1e1e] dark:text-slate-300 bg-gray-300 p-4 rounded-lg text-center">Опитай пак</button>
-          <ClientOnly>
-            <Teleport to="body">
-              <component :is="modal.component.value" :players="players" :currentQuote="currentQuote" :round="round" v-if="modal.show.value" @close="modal.hideModal" @newGame="newGame" @share="shareLink"/>
-            </Teleport>
-          </ClientOnly>
+        <div
+          class="lg:absolute mt-2 lg:mt-0 lg:top-0 lg:left-0"
+          v-if="showRoomId"
+        >
+          <div>Игра: {{ router.currentRoute.value.query.g }}</div>
+          <div v-for="player in players" :key="player.name">
+            🔴 {{ player.name }} - {{ player.score }}
+          </div>
         </div>
       </div>
+      <p class="dark:text-slate-300 text-center lg:text-right my-2">
+        Резултат: <span class="font-bold text-lg">{{ score }}</span>
+      </p>
+      <p
+        class="dark:bg-[#1f1e1e] dark:text-slate-300 bg-gray-200 text-center w-full mx-auto px-4 py-2 mb-10"
+      >
+        "{{ currentQuote?.quote }}"
+      </p>
+      <ClientOnly
+        ><div class="grid grid-cols-1 lg:grid-cols-2 gap-4">
+          <button
+            :ref="'button' + person?.id"
+            @click="select(person?.id as number)"
+            v-for="person in currentQuotePair"
+            :key="person?.id"
+            :disabled="
+              selectedPerson != null ||
+              (hintUsed && person?.id != currentQuote?.said_by)
+            "
+            :class="{
+              'border-gray-700 dark:border-white': person?.id == selectedPerson,
+              '!border-red-500 !dark:border-[#752828]':
+                gameOver && person?.id == currentQuote?.said_by,
+              'dark:bg-[#1f1f1f] line-through transform scale-50':
+                hintUsed && person?.id != currentQuote?.said_by,
+            }"
+            class="border-2 transition duration-300 dark:border-gray-700 dark:hover:[&:not(:disabled)]:border-white hover:[&:not(:disabled)]:border-gray-700 border-gray-300 disabled:cursor-not-allowed disabled:text-gray-700 p-4 rounded-lg text-center dark:text-slate-300"
+          >
+            {{ person?.name }} -
+            <span>{{ person?.party }}</span>
+          </button>
+        </div>
+      </ClientOnly>
+      <div class="flex justify-center items-center mt-4">
+        <button
+          v-if="isCorrect"
+          @click="nextQuote"
+          class="dark:bg-[#287543] bg-green-600 text-white dark:text-slate-200 p-4 rounded-lg text-center"
+        >
+          Следващ цитат
+        </button>
+        <button
+          v-else-if="!isCorrect && gameOver"
+          @click="newGame"
+          class="dark:bg-[#1f1e1e] dark:text-slate-300 bg-gray-300 p-4 rounded-lg text-center"
+        >
+          Опитай пак
+        </button>
+      </div>
+      <ClientOnly>
+        <Teleport to="body">
+          <component
+            :is="modal.component.value"
+            :players="players"
+            :currentQuote="currentQuote"
+            :score="score"
+            v-if="modal.show.value"
+            @close="modal.hideModal"
+            @newGame="newGame"
+            @share="shareLink"
+          />
+        </Teleport>
+      </ClientOnly>
     </div>
-  </template>
-  
-  <script setup lang="ts">
-  import { event, query } from 'vue-gtag'
-  import { Database } from '~~/types/supabase';
-  // @ts-ignore
-  import { v4 as uuidv4 } from "uuid";
-  import ModalGameOver from '~~/components/ModalGameOver.vue';
-  import ModalSettings from '~~/components/ModalSettings.vue';
-  
-  const colorMode = useColorMode();
-  const route = useRoute();
-  const router = useRouter();
-  const client = useSupabaseClient<Database>();
-  const modal = useModal();
-  const { copy } = useClipboard();
-  const round = ref(0);
-  const currentQuote = computed(() => quotes.value ? quotes.value[round.value % 50] : null);
-  const selectedPerson: Ref<number | null> = ref(null)
-  const gameOver = ref(false)
-  const hintUsed = ref(false)
-  const { $socket, $nt } = useNuxtApp()  
-  const connected = ref(false)
-  const room = ref("");
-  const showRoomId = ref(false);
-  const players = ref([] as {name: string, score: number, isGameOver: boolean}[]);
-  
-  const isCorrect = computed(() => {
-    if (!selectedPerson.value) return false;
-    return selectedPerson.value === currentQuote.value?.said_by;
-  })
+  </div>
+</template>
 
-  const randomWrongPerson = computed(() => {
-    if(currentQuote.value && people.value) {
-      const wrongPeople = people.value.filter(person => person.id !== currentQuote.value?.said_by)
-      return wrongPeople[Math.floor(Math.random() * wrongPeople.length)]
-    }
-  })
+<script setup lang="ts">
+import { event, query } from "vue-gtag";
+import { Database } from "~~/types/supabase";
+// @ts-ignore
+import { v4 as uuidv4 } from "uuid";
+import ModalGameOver from "~~/components/ModalGameOver.vue";
+import ModalSettings from "~~/components/ModalSettings.vue";
+import { useStorage } from "@vueuse/core";
 
-  const quotes = computed(() => room.value!= "" ? quotesMultiplayer.value : quotesSupabase.value)
+const colorMode = useColorMode();
+const route = useRoute();
+const router = useRouter();
+const client = useSupabaseClient<Database>();
+const modal = useModal();
+const { copy } = useClipboard();
+const username = useStorage(
+  "username",
+  "Непознат играч №" +
+    Math.floor(Math.random() * 1000)
+      .toString()
+      .padStart(3, "0")
+);
+const round = ref(0);
+const score = ref(0);
+const currentQuote = computed(() =>
+  quotes.value ? quotes.value[round.value % 50] : null
+);
+const selectedPerson: Ref<number | null> = ref(null);
+const gameOver = ref(false);
+const hintUsed = ref(false);
+const { $socket, $nt } = useNuxtApp();
+const connected = ref(false);
+const room = ref("");
+const showRoomId = ref(false);
+const players = ref(
+  [] as { name: string; score: number; isGameOver: boolean }[]
+);
 
-  const quotesMultiplayer = ref([]);
-  
-  const { data: quotesSupabase, refresh } = await useAsyncData('quotes', async () =>
-    client.from('random_quotes').select('*').range(0,50), { transform: data => data.data}
-  )
-  
-  const { data: people } = await useAsyncData('people', async () =>
-    client.from('people').select('*'), { transform: data => data.data}
-  )
+const isCorrect = computed(() => {
+  if (!selectedPerson.value) return false;
+  return selectedPerson.value === currentQuote.value?.said_by;
+});
 
-  const openGameOver = () => {
-    modal.component.value = markRaw(ModalGameOver);
-    modal.showModal();
+const shuffle = (array: Array<unknown>) => {
+  for (let i = array.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
   }
+};
 
-  const openSettings = () => {
-    modal.component.value = markRaw(ModalSettings);
-    modal.showModal();
+const currentQuotePair = computed(() => {
+  const correctPersonId = currentQuote.value?.said_by as number;
+  const pair = [
+    randomWrongPerson.value ?? null,
+    people.value?.find((person) => person.id === correctPersonId) ?? null,
+  ];
+  shuffle(pair);
+  return pair;
+});
+
+const randomWrongPerson = computed(() => {
+  if (currentQuote.value && people.value) {
+    const wrongPeople = people.value.filter(
+      (person) => person.id !== currentQuote.value?.said_by
+    );
+    return wrongPeople[Math.floor(Math.random() * wrongPeople.length)];
   }
-  
-  const select = (person_id: number) => {
-    selectedPerson.value = person_id;
-    if(!isCorrect.value) {
-      gameOver.value = true;
-      $socket.emit('message', {
-        message: 'gameOver',
-        room: room.value,
-        score: round.value,
-        username: localStorage.getItem('username'),
-      })
-      openGameOver();
-    }
-    $socket.emit('message', {
-        message: 'correctAnswer',
-        room: room.value,
-        score: round.value,
-        username: localStorage.getItem('username'),
-    })
+});
+
+const quotes = computed(() =>
+  room.value != "" ? quotesMultiplayer.value : quotesSupabase.value
+);
+
+const quotesMultiplayer = ref([]);
+
+const { data: quotesSupabase, refresh } = await useAsyncData(
+  "quotes",
+  async () => client.from("random_quotes").select("*").range(0, 50),
+  { transform: (data) => data.data }
+);
+
+const { data: people } = await useAsyncData(
+  "people",
+  async () => client.from("people").select("*"),
+  { transform: (data) => data.data }
+);
+
+const openGameOver = () => {
+  modal.component.value = markRaw(ModalGameOver);
+  modal.showModal();
+};
+
+const openSettings = () => {
+  modal.component.value = markRaw(ModalSettings);
+  modal.showModal();
+};
+
+const select = (person_id: number) => {
+  selectedPerson.value = person_id;
+  if (!isCorrect.value) {
+    gameOver.value = true;
+    $socket.emit("message", {
+      message: "gameOver",
+      room: room.value,
+      score: score.value,
+      username: localStorage.getItem("username"),
+    });
+    openGameOver();
   }
-  
-  const nextQuote = () => {
-    if(round.value + 1 === 50){
-      refresh();
-    }
-    round.value++;
-    event('next_quote', { event_category: 'game', event_label: 'next_quote', value: round.value })
-    selectedPerson.value = null;
-    hintUsed.value = false;
-  }
-  
-  const newGame = () => {
-    modal.hideModal();
-    selectedPerson.value = null;
-    gameOver.value = false;
-    hintUsed.value = false;
-    round.value = 0;
+  score.value += hintUsed.value ? 5 : 10;
+  console.log("score", score.value);
+  $socket.emit("message", {
+    message: "correctAnswer",
+    room: room.value,
+    score: score.value,
+    username: localStorage.getItem("username"),
+  });
+};
+
+const nextQuote = () => {
+  if (round.value + 1 === 50) {
     refresh();
   }
+  round.value++;
+  event("next_quote", {
+    event_category: "game",
+    event_label: "next_quote",
+    value: round.value,
+  });
+  selectedPerson.value = null;
+  hintUsed.value = false;
+};
 
-  const toggleColorMode = () => {
-    colorMode.preference = colorMode.preference === 'dark' ? 'light' : 'dark';
+const newGame = () => {
+  if (room.value != "") {
+    goToHome();
   }
+  modal.hideModal();
+  selectedPerson.value = null;
+  gameOver.value = false;
+  hintUsed.value = false;
+  round.value = 0;
+  score.value = 0;
+  refresh();
+};
 
-  const useHint = () => {
-    if(people.value && !hintUsed.value && !gameOver.value) {
-      event('use_hint', { event_category: 'game', event_label: 'use_hint', value: round.value })
-      hintUsed.value = true;
+const goToHome = () => {
+  window.location.replace("/");
+};
+
+const toggleColorMode = () => {
+  colorMode.preference = colorMode.preference === "dark" ? "light" : "dark";
+};
+
+const useHint = () => {
+  if (people.value && !hintUsed.value && !gameOver.value) {
+    event("use_hint", {
+      event_category: "game",
+      event_label: "use_hint",
+      value: round.value,
+    });
+    hintUsed.value = true;
+  } else {
+    $nt.show("Вече използвахте подсказката за този цитат");
+  }
+};
+
+const linkGame = () => {
+  if (connected.value) {
+    if (route.query.g) {
+      copy(`${window.location.origin}/?g=${route.query.g}`);
+      toastr("Копирано в клипборда!");
     } else {
-      $nt.show('Вече използвахте подсказката за този цитат')
-    }
-  }
-
-  const linkGame = () => {
-    if(connected.value) {
-      if(route.query.g){
-        copy(`${window.location.origin}/?g=${route.query.g}`);
-        toastr('Копирано в клипборда!')
-      } else {
-        const gameId = uuidv4().split('-')[0];
-        router.replace({query: {g: gameId}});
-        $socket.emit('message', {
-          room: gameId,
-          message: 'create',
-          username: localStorage.getItem('username'),
-          data: {
-            quotes: quotesSupabase.value,
-          }
-        })
-        console.log(quotesSupabase.value);
-        copy(`${window.location.origin}/?g=${gameId}`);
-        toastr('Копирано в клипборда!')
-        showRoomId.value = true;
-        const username = localStorage.getItem('username');
-        if(username) {
-          players.value = [];
-          players.value.push({name: username, score: 0, isGameOver: false});
-        }
-      }
-    }
-  }
-
-  const shareLink = () => {
-    window.open("https://www.facebook.com/sharer/sharer.php?u=https://koi-go-kazva.vercel.app/&display=popup",'Share to Facebook','width=600,height=400');
-  }
-
-  const toastr = (message: string) => {
-    $nt.show({
-        content: message,
-        duration: 1000,
-        theme: {
-          containerClass: 'text-black',
-          wrapperClass: "dark:bg-blue-500 bg-gray-300"
-        }
-      })
-  }
-
-  onMounted(() => {
-    if(router.currentRoute.value.query.g) {
-      $socket.emit('message', {
-          room: router.currentRoute.value.query.g,
-          username: localStorage.getItem('username') ?? 'Анонимен ' + Math.floor(Math.random() * 1000),
-          message: 'join'
+      const gameId = uuidv4().split("-")[0];
+      router.replace({ query: { g: gameId } });
+      $socket.emit("message", {
+        room: gameId,
+        message: "create",
+        username: localStorage.getItem("username"),
+        data: {
+          quotes: quotesSupabase.value,
+        },
       });
-      room.value = router.currentRoute.value.query.g.toString();
+      copy(`${window.location.origin}/?g=${gameId}`);
+      toastr("Копирано в клипборда!");
       showRoomId.value = true;
+      score.value = 0;
+      const username = localStorage.getItem("username");
+      if (username) {
+        players.value = [];
+        players.value.push({ name: username, score: 0, isGameOver: false });
+      }
     }
-    $socket.on('connect', () => {
-      connected.value = true;
-    })
-    $socket.on('message', (message) => {
-      if(message.message == 'loadGameData'){
-        quotesMultiplayer.value = message.data.quotes;
+  }
+};
+
+const shareLink = () => {
+  window.open(
+    "https://www.facebook.com/sharer/sharer.php?u=https://koi-go-kazva.vercel.app/&display=popup",
+    "Share to Facebook",
+    "width=600,height=400"
+  );
+};
+
+const toastr = (message: string) => {
+  $nt.show({
+    content: message,
+    duration: 1000,
+    theme: {
+      containerClass: "text-black",
+      wrapperClass: "dark:bg-blue-500 bg-gray-300",
+    },
+  });
+};
+
+onMounted(() => {
+  if (router.currentRoute.value.query.g) {
+    $socket.emit("message", {
+      room: router.currentRoute.value.query.g,
+      username:
+        localStorage.getItem("username") ??
+        "Анонимен " + Math.floor(Math.random() * 1000),
+      message: "join",
+    });
+    room.value = router.currentRoute.value.query.g.toString();
+    showRoomId.value = true;
+  }
+  $socket.on("connect", () => {
+    connected.value = true;
+  });
+  $socket.on("message", (message) => {
+    if (message.message == "loadGameData") {
+      quotesMultiplayer.value = message.data.quotes;
+    } else if (message.message == "loadPlayers") {
+      players.value = message.data;
+      const username = localStorage.getItem("username");
+      if (
+        username &&
+        message.data.find((player: any) => player.name == username)?.isGameOver
+      ) {
+        gameOver.value = true;
+        openGameOver();
       }
-      else if (message.message == 'loadPlayers') {
-        players.value = message.data;
-        const username = localStorage.getItem('username');
-        if(username && message.data.find((player: any) => player.name == username)?.isGameOver) {
-          gameOver.value = true;
-          openGameOver();
-        }
-      }
-    })
-  })
+    }
+  });
+});
 </script>
